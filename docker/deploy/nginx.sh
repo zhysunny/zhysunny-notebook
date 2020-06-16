b79eff4bcef38ddd40d43fc85c93cc3ef2d34925
zhysunny <51784188+zhysunny@users.noreply.github.com>
2020-06-16 11:29:07 +0800

    Update nginx.sh

--- docker/deploy/nginx.sh
+++ docker/deploy/nginx.sh
@@ -10,4 +10,9 @@ RUN rm -rf /etc/yum.repos.d/*
 COPY CentOS-8-reg.repo /etc/yum.repos.d/CentOS-8-reg.repo
 ADD nginx-1.18.0.tar.gz ./
 
-RUN yum -y install gcc-c++
+RUN yum downgrade -y glibc glibc-common libstdc++ libselinux libsemanage libsepol krb5-libs && \
+    yum install -y zlib zlib-devel libselinux-devel libsepol-devel libkadm5 wget make gcc-c++ pcre pcre-devel openssl openssl-devel libtool && \
+    cd nginx-${NGINX_VERSION} && ./configure --prefix=/usr/local/src/nginx-${NGINX_VERSION} && make && make install && \
+    echo 'export PATH="$PATH:/usr/local/src/nginx-${NGINX_VERSION}/sbin"' >> ~/.bashrc && source ~/.bashrc && nginx -v
+
+ENTRYPOINT ["sleep", "36000"]
