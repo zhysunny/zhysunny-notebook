dc9f67f662902dca7b459f0ffaca57182f8ff43d
zhysunny <1065300897@qq.com>
2020-12-19 10:39:54 +0800

    zookeeper

--- docker/docker安装/zookeeper/uninstall.sh
+++ docker/docker安装/zookeeper/uninstall.sh
@@ -4,5 +4,7 @@
 # Date        : 2020/7/29 23:42
 # Description : 卸载redis
 
-docker stop zookeeper
-docker rm zookeeper
+docker stop zookeeper1
+docker rm zookeeper1
+docker stop zookeeper2
+docker rm zookeeper2
